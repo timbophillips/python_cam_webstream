a8cde56e48399e19cc3bfda62b0edf4b772348ff
timbophillips <timothy.john.phillips@gmail.com>
2022-01-26 11:51:36 +0800

    initial upload

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# create folders and copy files
+sudo mkdir /usr/local/lib/python_cam_webstream
+sudo cp ./python_cam_webstream.py /usr/local/lib/python_cam_webstream/
+sudo cp ./python_cam_webstream.service /etc/systemd/system
+
+# ownership and permissions
+sudo chown root:root /usr/local/lib/python_cam_webstream/python_cam_webstream.py
+sudo chmod 644 /usr/local/lib/python_cam_webstream/python_cam_webstream.py
+sudo chown root:root /etc/systemd/system/python_cam_webstream.service
+sudo chmod 644 /etc/systemd/system/python_cam_webstream.service
+
+# systemd systemctl commands
+sudo systemctl daemon-reload
+sudo systemctl enable python_cam_webstream
+sudo systemctl restart python_cam_webstream
+sudo systemctl --property=MainPID show python_cam_webstream
